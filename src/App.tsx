f3f86d73f6b7a1eafc9cc766883b11c03a70a5de
Mohamed Ali <a.mohamed.cali@gmail.com>
2022-07-10 21:44:13 +0200

    Create add new items compoent

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { AddNewItem } from "./components/AddNewItem";
 import { Column } from "./components/Column";
 import { AppContainer } from "./styles";
 
@@ -5,6 +6,10 @@ export const App = () => {
   return (
     <AppContainer>
       <Column text="Todo:" />
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={() => console.log("Item created")}
+      />
     </AppContainer>
   );
 };
