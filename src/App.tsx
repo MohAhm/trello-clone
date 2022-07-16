d09196663b701f358dd29247e643a2c5efc2c734
Mohamed Ali <a.mohamed.cali@gmail.com>
2022-07-16 11:03:33 +0200

    Implement the global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { AddNewItem } from "./components/AddNewItem";
 import { Column } from "./components/Column";
+import { useAppState } from "./state/AppSateContext";
 import { AppContainer } from "./styles";
 
 export const App = () => {
+  const { lists } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="Todo:" />
+      {lists.map((list) => (
+        <Column key={list.id} id={list.id} text={list.text} />
+      ))}
       <AddNewItem
         toggleButtonText="+ Add another list"
         onAdd={() => console.log("Item created")}
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,18 +1,24 @@
+import { useAppState } from "../state/AppSateContext";
 import { ColumnContainer, ColumnTitle } from "../styles";
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
 
 type ColumnProps = {
+  id: string;
   text: string;
 };
 
-export const Column = ({ text }: ColumnProps) => {
+export const Column = ({ text, id }: ColumnProps) => {
+  const { getTaskByListId } = useAppState();
+
+  const tasks = getTaskByListId(id);
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <Card text="Generate app scaffold" />
-      <Card text="Learn TypeScript" />
-      <Card text="Begin to use static typing" />
+      {tasks.map((task) => (
+        <Card key={task.id} id={task.id} text={task.text} />
+      ))}
       <AddNewItem
         toggleButtonText="+ Add another card"
         onAdd={() => console.log("Card created")}
