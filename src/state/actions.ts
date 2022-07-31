df9cc8a6bb568d1618de6de385eb8f8e28bdb6c6
Mohamed Ali <a.mohamed.cali@gmail.com>
2022-07-31 19:30:31 +0200

    Define a utility function to move items

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,6 +1,7 @@
 export type Action = 
   | { type: "ADD_LIST"; payload: string }
   | { type: "ADD_TASK"; payload: { text: string; listId: string } }
+  | { type: "MOVE_LIST"; payload: { draggedId: string; hoverId: string }}
 
 
 export const addTask = (text: string, listId: string): Action => ({
@@ -14,4 +15,12 @@ export const addTask = (text: string, listId: string): Action => ({
 export const addList = (text: string): Action => ({
   type: "ADD_LIST",
   payload: text
+})
+
+export const moveList = (draggedId: string, hoverId: string): Action => ({
+  type: "MOVE_LIST",
+  payload: {
+    draggedId,
+    hoverId
+  }
 })
--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import { findItemIndexById } from "../utils/arrayUtils";
+import { findItemIndexById, moveItem } from "../utils/arrayUtils";
 import { Action } from "./actions";
 
 export type Task = {
@@ -35,6 +35,14 @@ export const appStateReducer = (draft: AppState, action: Action): AppState | voi
         id: nanoid(),
         text
       })
+      break
+    }
+    case "MOVE_LIST": {
+      const { draggedId, hoverId } = action.payload
+      const dragIndex = findItemIndexById(draft.lists, draggedId)
+      const hoverIndex = findItemIndexById(draft.lists, hoverId)
+      draft.lists = moveItem(draft.lists, dragIndex, hoverIndex)
+      break
     }
    }
 }
